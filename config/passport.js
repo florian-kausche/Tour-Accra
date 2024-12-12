8169cadce14d718d9ac28bb235fbe6135cdf2ac9
floriankausche <floriankausche@byui.edu>
2024-12-12 08:30:19 -0500

    tour accra

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ passport.use(new GitHubStrategy({
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
   callbackURL: process.env.GITHUB_CALLBACK_URL
 },
-async (accessToken, refreshToken, profile, done) => {
+async (_accessToken, refreshToken, profile, done) => {
   try {
     const db = getDatabase();
     const usersCollection = db.collection('users');
