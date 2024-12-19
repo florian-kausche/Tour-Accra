5877bdbcab5f0b9a03412c9300a2567e37b97aa5
floriankausche <floriankausche@byui.edu>
2024-12-19 19:10:22 +0000

    accra tour

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const isAuthenticated = (req, res, next) => {
     return next(); // User is authenticated, allow access to the protected area
   }
   // If not authenticated, redirect to the login page
-  res.redirect('/');
+  res.redirect('/places');
 };
 
 module.exports = {
