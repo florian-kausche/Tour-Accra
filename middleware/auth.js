ee2b5db69d0e625bd4fe8ccdf59b079d74bf2f06
floriankausche <floriankausche@byui.edu>
2024-12-20 04:23:18 +0000

    Accra Tour

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { isAuthenticated } = require('places');
+
 
 router.get('/protected', isAuthenticated, (req, res) => {
   res.status(200).json({ 
